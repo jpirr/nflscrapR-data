7cc17378381e96e349cb111ccdd7ea160271730d
John Pirrmann <johnpirrmann@Johns-MacBook-Pro-6.local>
2018-11-04 12:46:35 -0500

    app

---
+++ Final Project App.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(shiny)
+library(dplyr)
+library(knitr)
+library(tidytext)
+plays.16 <- read_csv("play_by_play_data/regular_season/reg_pbp_2016.csv")
+
+
+
+ui <- fluidPage(
+  titlePanel("2016 Passing Summary"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "passer_player_name",
+                  label = "Passer", 
+                  choices = c("E.Manning" = "Eli Manning", 
+                              "T.Brady" = "Tom Brady", 
+                              "A.Rodgers" = "Aaron Rodgers"))
+    ), 
+      mainPanel(
+        plotOutput(outputId = "scatter")
+      )
+    )
+  )
+  
+
+
+server <- function(input, output) {
+  output$scatter <- renderPlot({
+    ggplot(data = plays.16, aes(x = yards_gained, y = pass_location )) + geom_point()
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
