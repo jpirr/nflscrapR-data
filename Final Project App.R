68314e2a973d7c4957afd6cb15b993a18f612b4b
John Pirrmann <johnpirrmann@Johns-MacBook-Pro-6.local>
2018-11-05 08:01:33 -0500

    Attemp 2

--- Final Project App.R
+++ Final Project App.R
@@ -14,8 +14,7 @@ ui <- fluidPage(
       selectInput(inputId = "passer_player_name",
                   label = "Passer", 
                   choices = c("E.Manning" = "Eli Manning", 
-                              "T.Brady" = "Tom Brady", 
-                              "A.Rodgers" = "Aaron Rodgers"))
+                              "T.Brady" = "Tom Brady"))
     ), 
       mainPanel(
         plotOutput(outputId = "scatter")
@@ -27,6 +26,8 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$scatter <- renderPlot({
+    plays.16 <- read_csv("play_by_play_data/regular_season/reg_pbp_2016.csv") %>% 
+      filter(posteam =="NYG" | posteam =="NE")
     ggplot(data = plays.16, aes(x = yards_gained, y = pass_location )) + geom_point()
   })
   
